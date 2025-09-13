d1ec9f09240d76bc2096251fdab74521dd38e2f8
nimbachi <nimbachi@unicauca.edu.co>
2025-09-13 18:37:23 -0500

    Adding security

--- src/main/java/debt_payments/infraestructure/output/jpa/repository/replicas/IInvoiceRepository.java
+++ src/main/java/debt_payments/infraestructure/output/jpa/repository/replicas/IInvoiceRepository.java
@@ -1,11 +1,9 @@
 package debt_payments.infraestructure.output.jpa.repository.replicas;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import debt_payments.infraestructure.output.jpa.entity.replicas.InvoiceReplicaEntity;
 
 @Repository
---
+++ src/main/java/debt_payments/infraestructure/output/security/IJwtUtils.java
@@ -0,0 +1,5 @@
+package debt_payments.infraestructure.output.security;
+
+public interface IJwtUtils {
+    String getId();
+}
