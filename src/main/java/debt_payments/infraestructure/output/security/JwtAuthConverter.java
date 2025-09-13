23c9aa6205ac5d2300444abd70752a385e6e0c59
nimbachi <nimbachi@unicauca.edu.co>
2025-09-13 18:56:23 -0500

    Adding security

--- src/main/java/debt_payments/infraestructure/output/security/JwtAuthConverter.java
+++ src/main/java/debt_payments/infraestructure/output/security/JwtAuthConverter.java
@@ -14,7 +14,9 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimNames;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken>, IJwtUtils {
 
     private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
